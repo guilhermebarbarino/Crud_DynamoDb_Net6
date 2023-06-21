38625e40f104366081c6509ce710de677ba7cb57
guilherme <guiilhermesilva466@gmail.com>
2023-06-20 21:28:03 -0300

    projeto

---
+++ Dynamo_Manager/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Dynamo_Manager.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dynamo_Manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //injeção de dependencia
+        private readonly IDynamoDBContext _context;
+        public StudentsController(IDynamoDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetById(int studentId)
+        {
+            var student = await _context.LoadAsync<Students>(studentId);
+            if (student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var student = await _context.ScanAsync<Students>(default).GetRemainingAsync();
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStudent(Students students) 
+        {
+            var student = await _context.LoadAsync<Students>(students.Id);
+            if (student != null) return BadRequest($"Student with Id {student.Id} Already Exists");
+            await _context.SaveAsync(students);
+            return Ok(student);
+        }
+
+        [HttpDelete("{studentId}")]
+        public async Task<IActionResult> DeleteStudent(int studentId)
+        {
+            var student = await _context.LoadAsync<Students>(studentId);
+            if (student == null) return NotFound();
+            await _context.DeleteAsync(student);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateStudent(Students students)
+        {
+            var student = await _context.LoadAsync<Students>(students.Id);
+            if (student == null) return NotFound();
+            await _context.SaveAsync(students);
+            return Ok(students);
+        }
+        
+    }
+}
---
+++ Dynamo_Manager/models/Students.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Dynamo_Manager.models
+{
+
+    [DynamoDBTable("students")]
+    public class Students
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+
+        [DynamoDBProperty("Nome")]
+        public string? Nome { get; set; }
+
+        [DynamoDBProperty("Idade")]
+        public int? Idade { get; set; }
+
+        [DynamoDBProperty("Pais")]
+        public string? Pais { get; set; }
+    }
+}
